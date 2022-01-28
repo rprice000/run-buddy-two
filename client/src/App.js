defa14f973b3b62037c163b4b41bf1a35a303ddb
Lesly <leslyr562@gmail.com>
2022-01-27 18:34:15 -0600

    added front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 // You will need to import components here
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+//connection to the back-end server's /graphql
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
-    <div> Hello World</div>
+    <ApolloProvider client={client}>
+      <div >
+        Hello World
+      </div>
+    </ApolloProvider>
   );
 }
 
